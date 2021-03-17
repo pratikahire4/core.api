3c08894aa512a75ef97ba968f55229751eeb5c2e
Pratik Ahire <ahirepratik4.94@gmail.com>
2021-03-17 11:17:03 +0530

    core api basic solution checkin

---
+++ Core.Api/Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        [HttpPost]
+        public IActionResult CheckStatus()
+        {
+            return Ok("Working as expected");
+        }
+    }
+}
